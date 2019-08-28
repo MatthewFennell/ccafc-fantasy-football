d5e12af760c50104dda1d9e676d03e21058d2185
MattFennell <MFennell@scottlogic.com>
2019-08-28 10:22:48 +0100

    Sign in with google and facebook

--- src/components/auth/SignIn.js
+++ src/components/auth/SignIn.js
@@ -15,10 +15,7 @@ class SignIn extends Component {
     signInFlow: "popup",
     signInOptions: [
       firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-      firebase.auth.FacebookAuthProvider.PROVIDER_ID,
-      firebase.auth.TwitterAuthProvider.PROVIDER_ID,
-      firebase.auth.GithubAuthProvider.PROVIDER_ID,
-      firebase.auth.EmailAuthProvider.PROVIDER_ID
+      firebase.auth.FacebookAuthProvider.PROVIDER_ID
     ],
     callbacks: {
       signInSuccess: a => console.log("heya", a)
