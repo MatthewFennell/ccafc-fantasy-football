16797e7b9767bbb6595ccd199deac2de72e24289
MattFennell <MFennell@scottlogic.com>
2019-11-11 10:05:58 +0000

    Config setup

--- src/helperFunctions.js
+++ src/helperFunctions.js
@@ -1,6 +1,5 @@
 import * as constants from './constants';
 
 export const isDefensive = position => position.toLowerCase() === 'goalkeeper' || position.toLowerCase() === 'defender';
-
 export const generatePointsRoute = (userId, week) => `${constants.URL.POINTS}/${userId}/${week}`;
 export const generateOverviewRoute = (userId, week) => `${constants.URL.OVERVIEW}/${userId}/${week}`;
