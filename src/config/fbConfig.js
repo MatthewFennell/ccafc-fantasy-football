6d75358cf3bc26904a5ef77580f1c5d727afd857
Matt <mfennell@scottlogic.com>
2019-12-17 17:49:26 +0000

    testing dep

--- src/config/fbConfig.js
+++ src/config/fbConfig.js
@@ -4,15 +4,16 @@ import ReduxSagaFirebase from 'redux-saga-firebase';
 
 console.log('env stuff', process.env);
 
+
 const firebaseApp = firebase.initializeApp({
-    apiKey: process.env.REACT_APP_API_KEY,
-    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-    databaseURL: process.env.REACT_APP_DATABASE_URL,
-    projectId: process.env.REACT_APP_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-    appId: process.env.REACT_APP_APP_ID,
-    measurementId: process.env.REACT_APP
+    apiKey: 'AIzaSyDU5vZfbt0_5aR7BgZxjD32ZvrtwBonbHI',
+    authDomain: 'ccafc-fantasy-football.firebaseapp.com',
+    databaseURL: 'https://ccafc-fantasy-football.firebaseio.com',
+    projectId: 'ccafc-fantasy-football',
+    storageBucket: 'ccafc-fantasy-football.appspot.com',
+    messagingSenderId: '568615767647',
+    appId: '1:568615767647:web:814ed56750d0f56cf4d5ea',
+    measurementId: 'G-24WBM4W3B1'
 });
 const rsf = new ReduxSagaFirebase(firebaseApp);
 rsf.region = 'europe-west2';
