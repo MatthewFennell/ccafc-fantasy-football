21dc670887d157fed8e8d50d492b12676b77ad55
Matt <mfennell@scottlogic.com>
2020-02-08 23:27:26 +0000

    Feature request reducer

--- src/fixtures/reducer.test.js
+++ src/fixtures/reducer.test.js
@@ -1,7 +1,6 @@
 import reducer, { initialState } from './reducer';
 import * as actions from './actions';
 
-
 describe('Fixtures reducer', () => {
     it('should return the initial state', () => {
         expect(reducer(undefined, {})).toEqual(initialState);
