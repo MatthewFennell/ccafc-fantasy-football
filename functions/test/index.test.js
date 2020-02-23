74e10d4f63b488637aca1d706ea4d39a81efbf4b
Matt <mfennell@scottlogic.com>
2020-02-23 12:48:50 +0000

    All

---
+++ functions/test/index.test.js
@@ -0,0 +1,5 @@
+const test = require('firebase-functions-test')({
+    databaseURL: 'https://ccafc-fantasy-football-test.firebaseio.com',
+    storageBucket: 'ccafc-fantasy-football-test.appspot.com',
+    projectId: 'ccafc-fantasy-football-test'
+}, 'path/to/serviceAccountKey.json');
