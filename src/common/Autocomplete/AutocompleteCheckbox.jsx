c399d5ac6cb29fc0b4b0a8982ba062d01ad7d2e8
Matt <mfennell@scottlogic.com>
2020-03-04 18:30:11 +0000

    autocomplete is controlled

--- src/common/Autocomplete/AutocompleteCheckbox.jsx
+++ src/common/Autocomplete/AutocompleteCheckbox.jsx
@@ -9,6 +9,7 @@ import CheckBoxIcon from '@material-ui/icons/CheckBox';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
 import CircularProgress from '@material-ui/core/CircularProgress';
+import { noop } from 'lodash';
 import defaultStyles from './Autocomplete.module.scss';
 
 const icon = <CheckBoxOutlineBlankIcon fontSize="small" />;
@@ -22,9 +23,11 @@ const AutocompleteCheckbox = props => (
         <Autocomplete
             multiple
             id="checkboxes-tags-demo"
-            options={top100Films}
+            options={props.options}
             disableCloseOnSelect
             getOptionLabel={option => option.title}
+            onChange={(e, v) => props.onChange(v)}
+            value={props.value}
             loading={props.loading}
             renderOption={(option, { selected }) => (
                 <>
@@ -37,13 +40,13 @@ const AutocompleteCheckbox = props => (
                     {option.title}
                 </>
             )}
-            style={{ width: 500 }}
             renderInput={params => (
                 <TextField
                     {...params}
                     variant="outlined"
                     label="Checkboxes"
                     placeholder="Favorites"
+
                     InputProps={{
                         ...params.InputProps,
                         endAdornment: (
@@ -67,51 +70,28 @@ const AutocompleteCheckbox = props => (
 AutocompleteCheckbox.propTypes = {
     loading: PropTypes.bool,
     loadingColor: PropTypes.string,
-    withPadding: PropTypes.bool,
-    styles: PropTypes.objectOf(PropTypes.string)
+    onChange: PropTypes.func,
+    options: PropTypes.arrayOf(PropTypes.shape({
+        checked: PropTypes.bool,
+        title: PropTypes.string
+    })),
+    styles: PropTypes.objectOf(PropTypes.string),
+    value: PropTypes.arrayOf(PropTypes.shape({
+        checked: PropTypes.bool,
+        title: PropTypes.string
+    })),
+    withPadding: PropTypes.bool
 };
 
 AutocompleteCheckbox.defaultProps = {
     loading: false,
     loadingColor: 'primary',
-    withPadding: false,
-    styles: defaultStyles
+    onChange: noop,
+    options: [],
+    styles: defaultStyles,
+    value: [],
+    withPadding: false
 };
 
-// Top 100 films as rated by IMDb users. http://www.imdb.com/chart/top
-const top100Films = [
-    { title: 'The Shawshank Redemption', year: 1994 },
-    { title: 'The Godfather', year: 1972 },
-    { title: 'The Godfather: Part II', year: 1974 },
-    { title: 'The Dark Knight', year: 2008 },
-    { title: '12 Angry Men', year: 1957 },
-    { title: 'Schindler\'s List', year: 1993 },
-    { title: 'Pulp Fiction', year: 1994 },
-    { title: 'The Lord of the Rings: The Return of the King', year: 2003 },
-    { title: 'The Good, the Bad and the Ugly', year: 1966 },
-    { title: 'Fight Club', year: 1999 },
-    { title: 'The Lord of the Rings: The Fellowship of the Ring', year: 2001 },
-    { title: 'Star Wars: Episode V - The Empire Strikes Back', year: 1980 },
-    { title: 'Forrest Gump', year: 1994 },
-    { title: 'Inception', year: 2010 },
-    { title: 'The Lord of the Rings: The Two Towers', year: 2002 },
-    { title: 'One Flew Over the Cuckoo\'s Nest', year: 1975 },
-    { title: 'Goodfellas', year: 1990 },
-    { title: 'The Matrix', year: 1999 },
-    { title: 'Seven Samurai', year: 1954 },
-    { title: 'Star Wars: Episode IV - A New Hope', year: 1977 },
-    { title: 'City of God', year: 2002 },
-    { title: 'Se7en', year: 1995 },
-    { title: 'The Silence of the Lambs', year: 1991 },
-    { title: 'It\'s a Wonderful Life', year: 1946 },
-    { title: 'Life Is Beautiful', year: 1997 },
-    { title: 'The Usual Suspects', year: 1995 },
-    { title: 'Léon: The Professional', year: 1994 },
-    { title: 'Spirited Away', year: 2001 },
-    { title: 'Saving Private Ryan', year: 1998 },
-    { title: 'Once Upon a Time in the West', year: 1968 },
-    { title: 'American History X', year: 1998 },
-    { title: 'Interstellar', year: 2014 }
-];
 
 export default AutocompleteCheckbox;
