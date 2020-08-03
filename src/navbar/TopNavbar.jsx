05153b751dc517181d0e6c45ba4a412ae0331c6c
Matt <mfennell@scottlogic.com>
2020-08-03 18:50:25 +0100

    Caching

--- src/navbar/TopNavbar.jsx
+++ src/navbar/TopNavbar.jsx
@@ -69,7 +69,7 @@ const TopNavbar = props => {
                     <MenuIcon />
                 </IconButton>
                 <Typography variant="h6" className={classes.title}>
-                    CCAFC Fantasy
+                    CCAFC Fantasy 1
                 </Typography>
                 <div>
                     <IconButton
