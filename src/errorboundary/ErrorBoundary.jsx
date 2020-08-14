96baf3541f6870938222062cca79c502a92d03ef
Matt <mfennell@scottlogic.com>
2020-08-14 21:46:28 +0100

    fix tests

--- src/errorboundary/ErrorBoundary.jsx
+++ src/errorboundary/ErrorBoundary.jsx
@@ -14,7 +14,7 @@ class ErrorBoundary extends React.Component {
     }
 
     render() {
-        if (this.state.hasError || true) {
+        if (this.state.hasError) {
         // You can render any custom fallback UI
             return (
                 <div className={this.props.styles.errorMessageWrapper}>
