a5c6d768ae172609517a7b74cdbc1ec38013604b
Matt <mfennell@scottlogic.com>
2020-09-11 22:11:26 +0100

    Fix tests

--- src/common/fadingCollapsable/FadingCollapsable.jsx
+++ src/common/fadingCollapsable/FadingCollapsable.jsx
@@ -26,13 +26,14 @@ const FadingCollapsable = props => {
             <Fade
                 checked={isOpen}
             >
-                {/* {props.children} */}
-                {React.cloneElement(props.children, { setIsCollapsableOpen: setIsOpen })}
+                {props.children
+                && React.cloneElement(props.children, { setIsCollapsableOpen: setIsOpen })}
             </Fade>
             <Fade
                 checked={!isOpen}
             >
-                {React.cloneElement(props.title, { setIsCollapsableOpen: setIsOpen })}
+                {props.title
+                && React.cloneElement(props.title, { setIsCollapsableOpen: setIsOpen })}
             </Fade>
         </div>
     );
